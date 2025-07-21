84e6d82f37d0e16a4ef46299b12f7689f4c03988
Abdellatif Alrefahi <abdellatif.alrefahi@gmail.com>
2025-07-21 16:49:55 +0300

    Fix import schemas

--- db/db.ts
+++ db/db.ts
@@ -1,15 +1,16 @@
 // Local DB
 // import { drizzle } from "drizzle-orm/node-postgres";
 // import { env } from "@/data/env/server";
-// import * as schema from "@/db/schema";
 
 // Neon DB
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 
+import * as schema from "@/db/schema";
+
 const sql = neon(process.env.DATABASE_URL!);
 
-export const db = drizzle({ client: sql });
+export const db = drizzle({ client: sql, schema });
 
 // Local DB
 // export const db = drizzle(env.DATABASE_URL, { schema });
